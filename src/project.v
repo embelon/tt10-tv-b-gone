22ca3beb1d382f064aa8de3f1decb55d38a872eb
embelon <78412338+embelon@users.noreply.github.com>
2025-02-26 21:38:09 +0100

    Updated top with interface and unused ios

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_tv_b_gone (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,12 +16,21 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+
+  // ui_in[0] => button
+  // uo_out[0] => IR LED
+  // uo_out[1] => activity LED
+
+
+
+
+
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uo_out[7]  = ui_in[7] + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uio_out = 8'h00;
+  assign uio_oe  = 8'h00;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, ui_in[6:1], uo_out[6:2], uio_in[7:0], 1'b0};
 
 endmodule
